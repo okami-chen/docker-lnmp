698c8fd719fc58a4cc7aae214abd6bb81e96281d
okamichen <webmaster@88188.live>
2021-06-25 21:54:14 +0800

    Update ext.sh

--- basic/ext.sh
+++ basic/ext.sh
@@ -1,4 +1,4 @@
-apt-get -y install supervisor \
+apt-get -y install supervisor rsyslog crond\
 && cd /tmp && wget https://github.com/phpredis/phpredis/archive/refs/tags/5.3.4.zip \
 && unzip 5.3.4.zip && cd phpredis-5.3.4 \
 && phpize && ./configure --with-php-config=/usr/local/php/bin/php-config \
@@ -10,7 +10,8 @@ apt-get -y install supervisor \
 && phpize && ./configure --with-php-config=/usr/local/php/bin/php-config --enable-openssl --enable-sockets --enable-mysqlnd --enable-http2 \
 && make && make install \
 && echo "extension=swoole.so" > /usr/local/php/conf.d/swoole.ini \
-&& rm -rf /var/cache/apk/* /tmp/* /usr/share/man /var/lib/apt/lists/*
+&& rm -rf /var/cache/apk/* /tmp/* /usr/share/man /var/lib/apt/lists/* \
+&& sed -i '/s/session/#session/g' /etc/pam.d/crond
 
 # && sed -i '/s/php-7.4.12/php-7.4.19/g' /tmp/lnmp1.7/include/version.sh \
 # && sed -i "" "s/disable_functions/#disable_functions/g" /usr/local/php/etc/php.ini \
