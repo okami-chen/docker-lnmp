0511eb983d86193c4550b52d7d2e753ee2531545
okamichen <webmaster@88188.live>
2021-06-26 11:08:34 +0800

    Update ext.sh

--- basic/ext.sh
+++ basic/ext.sh
@@ -1,4 +1,4 @@
-apt-get -y install supervisor rsyslog crond \
+apt-get -y install supervisor rsyslog cron \
 && cd /tmp && wget https://github.com/phpredis/phpredis/archive/refs/tags/5.3.4.zip \
 && unzip 5.3.4.zip && cd phpredis-5.3.4 \
 && phpize && ./configure --with-php-config=/usr/local/php/bin/php-config \
