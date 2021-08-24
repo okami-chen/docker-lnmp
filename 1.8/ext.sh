aab93f2509ba6b238ab2447b8e3214591576ab07
okamichen <webmaster@88188.live>
2021-08-24 17:04:44 +0800

    Update ext.sh

--- 1.8/ext.sh
+++ 1.8/ext.sh
@@ -7,7 +7,7 @@ apt-get -y install supervisor rsyslog cron \
 && wget https://github.com/swoole/swoole-src/archive/master.tar.gz \
 && tar zxvf master.tar.gz \
 && mv swoole-src* swoole-src && cd swoole-src \
-&& phpize && ./configure --with-php-config=/usr/local/php/bin/php-config --enable-openssl --enable-sockets --enable-mysqlnd --enable-http2 \
+&& phpize && ./configure --with-php-config=/usr/local/php/bin/php-config --enable-openssl --enable-sockets --enable-mysqlnd --enable-http2 --enable-swoole-json --enable-swoole-curl \
 && make && make install \
 && echo "extension=swoole.so" > /usr/local/php/conf.d/swoole.ini \
 && rm -rf /var/cache/apk/* /tmp/* /usr/share/man /var/lib/apt/lists/*
