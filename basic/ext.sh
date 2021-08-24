b07987f43dbb0df1930a68d27dcdefdd7cd50299
okamichen <webmaster@88188.live>
2021-08-24 17:04:03 +0800

    Update ext.sh

--- basic/ext.sh
+++ basic/ext.sh
@@ -7,11 +7,10 @@ apt-get -y install supervisor rsyslog cron \
 && wget https://github.com/swoole/swoole-src/archive/master.tar.gz \
 && tar zxvf master.tar.gz \
 && mv swoole-src* swoole-src && cd swoole-src \
-&& phpize && ./configure --with-php-config=/usr/local/php/bin/php-config --enable-openssl --enable-sockets --enable-mysqlnd --enable-http2 \
+&& phpize && ./configure --with-php-config=/usr/local/php/bin/php-config --enable-openssl --enable-sockets --enable-mysqlnd --enable-http2 --enable-swoole-json --enable-swoole-curl \
 && make && make install \
 && echo "extension=swoole.so" > /usr/local/php/conf.d/swoole.ini \
-&& rm -rf /var/cache/apk/* /tmp/* /usr/share/man /var/lib/apt/lists/* \
-&& sed -i 's/session/#session/g' /etc/pam.d/crond
+&& rm -rf /var/cache/apk/* /tmp/* /usr/share/man /var/lib/apt/lists/*  
 
 # && sed -i 's/php-7.4.12/php-7.4.19/g' /tmp/lnmp1.7/include/version.sh \
 # && sed -i "" "s/disable_functions/#disable_functions/g" /usr/local/php/etc/php.ini \
