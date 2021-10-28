dbfffbeb5cb5f93e0122032f8e12de9434adb859
陈德华 <chendehua@51xpx.com>
2021-10-28 17:02:34 +0800

    * update

--- 8.0/shell/amqp.sh
+++ 8.0/shell/amqp.sh
@@ -2,4 +2,4 @@ cd /tmp && tar xvf amqp-${AMQP_VERSION}.tgz && cd /tmp/amqp-${AMQP_VERSION} \
 && /usr/local/php/bin/phpize \
 && /tmp/amqp-1.11.0beta/configure --with-php-config=/usr/local/php/bin/php-config \
 && make && make install \
-&& echo "extension=amqp.so" > /usr/local/php/conf.d/amqp.ini
+&& echo "extension=amqp.so" > /usr/local/php/conf.d/03_amqp.ini
--- 8.0/shell/swoole.sh
+++ 8.0/shell/swoole.sh
@@ -3,4 +3,4 @@ cd /tmp && wget https://github.com/swoole/swoole-src/archive/master.tar.gz \
 && mv swoole-src* swoole-src && cd swoole-src \
 && phpize && ./configure --with-php-config=/usr/local/php/bin/php-config --enable-openssl --enable-sockets --enable-mysqlnd --enable-http2 --enable-swoole-json --enable-swoole-curl \
 && make && make install \
-&& echo "extension=swoole.so" > /usr/local/php/conf.d/swoole.ini
+&& echo "extension=swoole.so" > /usr/local/php/conf.d/02_swoole.ini
