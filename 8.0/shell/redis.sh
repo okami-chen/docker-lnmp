0757b5f4dc04bc2fb155b17a139932b4904a849b
陈德华 <chendehua@51xpx.com>
2021-10-28 17:08:15 +0800

    * update Dockerfile

--- 8.0/shell/redis.sh
+++ 8.0/shell/redis.sh
@@ -1,5 +1,4 @@
-cd /tmp && wget https://github.com/phpredis/phpredis/archive/refs/tags/5.3.4.zip \
-&& unzip ${REDIS_VERSION}.zip && cd phpredis-${REDIS_VERSION} \
+cd /tmp && unzip phpredis-${REDIS_VERSION}.zip && cd phpredis-${REDIS_VERSION} \
 && phpize && ./configure --with-php-config=/usr/local/php/bin/php-config \
 && make && make install \
 && echo "extension=redis.so" > /usr/local/php/conf.d/01_redis.ini
--- 8.0/shell/swoole.sh
+++ 8.0/shell/swoole.sh
@@ -1,5 +1,4 @@
-cd /tmp && wget https://github.com/swoole/swoole-src/archive/master.tar.gz \
-&& tar zxvf master.tar.gz \
+cd /tmp && tar zxvf swoole-master.tar.gz \
 && mv swoole-src* swoole-src && cd swoole-src \
 && phpize && ./configure --with-php-config=/usr/local/php/bin/php-config --enable-openssl --enable-sockets --enable-mysqlnd --enable-http2 --enable-swoole-json --enable-swoole-curl \
 && make && make install \
