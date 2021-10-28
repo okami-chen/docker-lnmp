53a06a3aa5eaf67b677d428f9a2a7cdd8e582ec5
陈德华 <chendehua@51xpx.com>
2021-10-28 17:18:00 +0800

    use swoole 4.8.0

--- 8.0/shell/swoole.sh
+++ 8.0/shell/swoole.sh
@@ -1,5 +1,5 @@
-cd /tmp && tar zxvf swoole-master.tar.gz \
-&& mv swoole-src* swoole-src && cd swoole-src \
+cd /tmp && tar zxvf swoole-${SWOOLE_VERSION}.tar.gz \
+&& cd swoole-src-${SWOOLE_VERSION} \
 && phpize && ./configure --with-php-config=/usr/local/php/bin/php-config --enable-openssl --enable-sockets --enable-mysqlnd --enable-http2 --enable-swoole-json --enable-swoole-curl \
 && make && make install \
 && echo "extension=swoole.so" > /usr/local/php/conf.d/02_swoole.ini
