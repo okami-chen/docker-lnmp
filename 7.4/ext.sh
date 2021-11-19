6a28c6915dc41482b6bf0c8d1c4ef08c83647cd6
陈德华 <chendehua@51xpx.com>
2021-11-19 11:28:01 +0800

    * bug fix

--- 7.4/ext.sh
+++ 7.4/ext.sh
@@ -1,6 +1,6 @@
 apt-get -y install supervisor rsyslog cron ssh \
-&& cd /tmp && wget https://github.com/phpredis/phpredis/archive/refs/tags/5.3.4.zip \
-&& unzip 5.3.4.zip && cd phpredis-5.3.4 \
+&& cd /tmp && wget https://github.com/phpredis/phpredis/archive/refs/tags/${REDIS_VERSION}.zip \
+&& unzip ${REDIS_VERSION}.zip && cd phpredis-${REDIS_VERSION} \
 && phpize && ./configure --with-php-config=/usr/local/php/bin/php-config \
 && make && make install \
 && echo "extension=redis.so" > /usr/local/php/conf.d/redis.ini \
--- 8.1/version.sh
+++ 8.1/version.sh
@@ -63,9 +63,9 @@ elif [ "${PHPSelect}" = "8" ]; then
 elif [ "${PHPSelect}" = "9" ]; then
     Php_Ver='php-7.3.28'
 elif [ "${PHPSelect}" = "10" ]; then
-    Php_Ver='php-7.4.21'
+    Php_Ver='php-7.4.26'
 elif [ "${PHPSelect}" = "11" ]; then
-    Php_Ver='php-8.1.0RC4'
+    Php_Ver='php-8.1.0RC6'
 fi
 if [[ "${PHPSelect}" =~ ^[123]$ ]]; then
     PhpMyAdmin_Ver='phpMyAdmin-4.0.10.20-all-languages'
