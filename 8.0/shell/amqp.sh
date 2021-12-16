4e0c4bbb7d20adf791827e43270110d9aee244b2
陈德华 <chendehua@51xpx.com>
2021-12-16 18:48:38 +0800

    * bug fix

--- 8.0/shell/amqp.sh
+++ 8.0/shell/amqp.sh
@@ -1,5 +1,5 @@
 cd /tmp && tar xvf amqp-${AMQP_VERSION}.tgz && cd /tmp/amqp-${AMQP_VERSION} \
 && /usr/local/php/bin/phpize \
-&& /tmp/amqp-1.11.0beta/configure --with-php-config=/usr/local/php/bin/php-config \
+&& /tmp/amqp-${AMQP_VERSION}/configure --with-php-config=/usr/local/php/bin/php-config \
 && make && make install \
 && echo "extension=amqp.so" > /usr/local/php/conf.d/03_amqp.ini
