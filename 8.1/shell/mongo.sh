48e4f76a4fdd839bb0506cffe565b740fc12a13c
陈德华 <chendehua@51xpx.com>
2021-12-18 10:55:10 +0800

    * update mongo.sh

--- 8.1/shell/mongo.sh
+++ 8.1/shell/mongo.sh
@@ -1,4 +1,4 @@
-cd /tmp && unzip mongodb-${MONGODB_VERSION}.zip && cd mongodb-${MONGODB_VERSION} \
+cd /tmp && unzip mongodb-${MONGODB_VERSION}.zip && cd mongo-php-driver-${MONGODB_VERSION} \
 && phpize && ./configure --with-php-config=/usr/local/php/bin/php-config \
 && make && make install \
 && echo "extension=mongodb.so" > /usr/local/php/conf.d/05_mongodb.ini
