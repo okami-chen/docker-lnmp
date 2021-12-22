8ada2fd4b4e8d9b240a2820a5982940b7dfa2aeb
陈德华 <chendehua@51xpx.com>
2021-12-22 11:47:02 +0800

    * tws

---
+++ tws/shell/mongo.sh
@@ -0,0 +1,4 @@
+cd /tmp && unzip mongodb-${MONGODB_VERSION}.zip && cd mongo-php-driver-${MONGODB_VERSION} \
+&& phpize && ./configure --with-php-config=/usr/local/php/bin/php-config \
+&& make && make install \
+&& echo "extension=mongodb.so" > /usr/local/php/conf.d/05_mongodb.ini
---
+++ tws/shell/ssh.sh
@@ -0,0 +1,2 @@
+mkdir -p /root/.ssh && cat /tmp/authorized_keys > /root/.ssh/authorized_keys \
+&& chmod 600 /root/.ssh/authorized_keys
