39b30fc80a415dd3d183838a100b4f6204ecc982
okamichen <webmaster@88188.live>
2022-07-14 09:37:48 +0800

    Update ext.sh

--- 7.4/ext.sh
+++ 7.4/ext.sh
@@ -4,7 +4,7 @@ apt-get -y install supervisor rsyslog cron ssh \
 && phpize && ./configure --with-php-config=/usr/local/php/bin/php-config \
 && make && make install \
 && echo "extension=redis.so" > /usr/local/php/conf.d/redis.ini \
-&& wget https://github.com/swoole/swoole-src/archive/master.tar.gz \
+&& cd /tmp && wget https://github.com/swoole/swoole-src/archive/master.tar.gz \
 && tar zxvf master.tar.gz \
 && mv swoole-src* swoole-src && cd swoole-src \
 && phpize && ./configure --with-php-config=/usr/local/php/bin/php-config --enable-openssl --enable-sockets --enable-mysqlnd --enable-http2 --enable-swoole-json --enable-swoole-curl \
